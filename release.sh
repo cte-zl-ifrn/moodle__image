c91bffe215a32818f09feca88b7e9987955ba82f
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2023-01-16 19:55:13 -0300

    build: upd: ordem dos parâmetros de release

--- release.sh
+++ release.sh
@@ -4,30 +4,32 @@ if [ $# -lt 3 ]; then
     echo 'NAME '
     echo '       release.sh'
     echo 'SYNOPSIS'
-    echo '       ./release.sh <operation> <build_version> <moodle_version>'
+    echo '       ./release.sh <operation> <moodle_version> <build_version>'
     echo 'DESCRIPTION'
     echo "       Build or push $FULL_IMAGE_NAME."
     echo 'OPTIONS'
     echo '       <operation>      Operation: build | push'
     echo '       <build_version>  Build version'
     echo 'EXAMPLES'
-    echo '       o   Build image:'
-    echo "                  ./release.sh build 001 401"
-    echo '       o   Push image:'
-    echo "                  ./release.sh push 001 401"
+    echo '       o   Build docker image:'
+    echo "                  ./release.sh build 401 001"
+    echo '       o   Push docker image and create git tag:'
+    echo "                  ./release.sh push 401 001"
     exit
 fi
 
 OPERATION=$1
-BUILD_VERSION=$2
-MOODLE_VERSION=$3
+MOODLE_VERSION=$2
+BUILD_VERSION=$3
 FULL_IMAGE_NAME="registry.gitlab.com/zl-installs/zl-ava-installs/moodle:$MOODLE_VERSION-$BUILD_VERSION"
 
 echo "$OPERATION $FULL_IMAGE_NAME"
 if [ $OPERATION == "build" ]; then  
     docker build . -f Dockerfile.$MOODLE_VERSION --progress plain -t $FULL_IMAGE_NAME
 elif [ $OPERATION == "push" ]; then
-    docker push $FULL_IMAGE_NAME
+    docker push $FULL_IMAGE_NAME \
+    && git tag $BUILD_VERSION \
+    && git push origin $BUILD_VERSION
 else
     echo "Choose a valid command (build or push)"
 fi
