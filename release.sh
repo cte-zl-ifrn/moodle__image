de8580486d1fea790b2fa5ce6ee2903171fc772b
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2023-03-23 14:01:21 -0300

    build: [fix] simplifica o release

--- release.sh
+++ release.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
-if [ $# -lt 3 ]; then
+if [ $# -lt 2 ]; then
     echo ''
     echo 'NAME '
     echo '       release.sh'
     echo 'SYNOPSIS'
     echo '       ./release.sh <operation> <moodle_version> <build_version>'
     echo 'DESCRIPTION'
-    echo "       Build or push $FULL_IMAGE_NAME."
+    echo "       Build or push image."
     echo 'OPTIONS'
     echo '       <operation>      Operation: build | push | redeploy'
     echo '       <build_version>  Build version'
     echo 'EXAMPLES'
     echo '       o   Build docker image:'
-    echo "                  ./release.sh build 401 010"
+    echo "                  ./release.sh build 030"
     echo '       o   Push docker image and create git tag:'
-    echo "                  ./release.sh push 401 010"
+    echo "                  ./release.sh push 030"
     echo '       o   Down, remove volumes, build docker image, recreate volumes, update docker-compose.yml and up:'
-    echo "                  ./release.sh redeploy 401 010"
+    echo "                  ./release.sh redeploy 030"
     exit
 fi
 
 OPERATION=$1
-MOODLE_VERSION=$2
-BUILD_VERSION=$3
+BUILD_VERSION=$2
+MOODLE_VERSION='401'
 FULL_IMAGE_NAME="registry.gitlab.com/zl-installs/zl-ava-installs/moodle:$MOODLE_VERSION-$BUILD_VERSION"
 
 echo "$OPERATION $FULL_IMAGE_NAME"
 if [ $OPERATION == "build" ]; then  
-    docker build . -f Dockerfile.$MOODLE_VERSION --progress plain -t $FULL_IMAGE_NAME \
+    docker build . -f Dockerfile --progress plain -t $FULL_IMAGE_NAME \
     && sed -i "s/moodle:.*$/moodle:$MOODLE_VERSION-$BUILD_VERSION/g" docker-compose.yml
 elif [ $OPERATION == "redeploy" ]; then
     docker compose down \
